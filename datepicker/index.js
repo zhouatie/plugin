1a66f074fad7106157d1aa4dfe77906ba4c31651
zhoust <zhoust@guahao.com>
2018-08-05 23:17:57 +0800

    开发下一个月功能中

--- datepicker/index.js
+++ datepicker/index.js
@@ -10,14 +10,14 @@
                 '<div class="ant-calendar-date-panel">'+
                     '<div class="ant-calendar-header">'+
                         '<div style="position: relative;">'+
-                            '<a class="ant-calendar-prev-year-btn" role="button" title="上一年 (Control键加左方向键)"></a>'+
-                            '<a class="ant-calendar-prev-month-btn" role="button" title="上个月 (翻页上键)"></a>'+
+                            '<a class="ant-calendar-prev-year-btn" role="button" title="上一年 (Control键加左方向键)">上一年</a>'+
+                            '<a class="ant-calendar-prev-month-btn" role="button" title="上个月 (翻页上键)">上个月</a>'+
                             '<span class="ant-calendar-ym-select">'+
                                 '<a class="ant-calendar-year-select" role="button" title="选择年份">2018年</a>'+
                                 '<a class="ant-calendar-month-select" role="button" title="选择月份">8月</a>'+
                             '</span>'+
-                            '<a class="ant-calendar-next-month-btn" title="下个月 (翻页下键)"></a>'+
-                            '<a class="ant-calendar-next-year-btn" title="下一年 (Control键加右方向键)"></a>'+
+                            '<a class="ant-calendar-next-month-btn" title="下个月 (翻页下键)">下个月</a>'+
+                            '<a class="ant-calendar-next-year-btn" title="下一年 (Control键加右方向键)">下一年</a>'+
                         '</div>'+
                     '</div>'+
                     '<div class="ant-calendar-body">'+
@@ -96,6 +96,11 @@
     function Calendar(opt) {
         // 参数
         this.opt = {};
+        var date = new Date();
+        this.dateOpt = {
+            _year: date.getFullYear(),
+            _month: date.getMonth()
+        }
         // 存储页面存在的calendar对象
         this.calendars = {};
 
@@ -114,33 +119,77 @@
         PANELSTR: 'calendar-panel_',
         PANELWRAPCLASS: 'calendar-wrap'
     }
-
+    Calendar.Target = null;  // 当前打开的日历视图
     Calendar.version = '1.0.0';
 
     Calendar.prototype = {
         constructor: Calendar,
         init: function() {
+            this.initEvent();
+        },
+        initEvent: function() {
             var self = this;
+            
+            Object.defineProperty(this.dateOpt, 'curYear', {
+                get: function() {
+                    return new Date().getFullYear();
+                },
+            })
+            Object.defineProperty(this.dateOpt, 'curMonth', {
+                get: function() {
+                    return new Date().getMonth();
+                },
+            })
+            Object.defineProperty(this.dateOpt, 'curDate', {
+                get: function() {
+                    return new Date().getDate();
+                },
+            })
+            Object.defineProperty(this.dateOpt, 'year', {
+                get: function() {
+                    return this._year;
+                },
+                set: function(newVal) {
+                    if (newVal === this._year) return;
+                    this._year = newVal;
+                    // self.render();
+                }
+            })
+            Object.defineProperty(this.dateOpt, 'month', {
+                get: function() {
+                    return this._month;
+                },
+                set: function(newVal) {
+                    if (newVal === this._month) return;
+                    this._month = newVal;
+                    self.render();
+                }
+            })
+            
             document.addEventListener('click', function(e) {
                 var target = e.target;
-                if (target.getAttribute('class') === self.opt.classN) {
+                if (utils.hasClass(target, self.opt.classN)) {
                     self.openPanel(target);
+                } else if (utils.hasClass(target, 'ant-calendar-next-month-btn')) {
+                    console.log(333)
+                    self.dateOpt.month++;
                 }
             }, false);
         },
-        getTemplate: function(year, month) {
-            // 当前日期对象
-            var current = new Date();
+        render: function() {
+            Calendar.Target.querySelector('tbody').innerHTML = this.getTemplate();
+        },
+        getTemplate: function() {
             // 当月第一天日期对象
-            var currentMonthFirstDateObj = new Date(year, month, 1);
+            var currentMonthFirstDateObj = new Date(this.dateOpt.year, this.dateOpt.month, 1);
             // 当月第一天星期
             var currentMonthFirstDay = currentMonthFirstDateObj.getDay();
             // 当月最后一天日期对象
-            var currentMonthLastDateObj = new Date(year, month+1, 0);
+            var currentMonthLastDateObj = new Date(this.dateOpt.year, this.dateOpt.month+1, 0);
             // 当月最后一天日期
             var currentMonthLastDay = currentMonthLastDateObj.getDate();
             // 上个月最后一天日期对象
-            var lastMonthLastDateObj = new Date(year, month, 0);
+            var lastMonthLastDateObj = new Date(this.dateOpt.year, this.dateOpt.month, 0);
             // 上个月最后一天日期
             var lastMonthLastDate = lastMonthLastDateObj.getDate();
             
@@ -154,7 +203,7 @@
                 } else if (i>currentMonthFirstDay+currentMonthLastDay-1) {
                     html += '<td class="ant-calendar-cell ant-calendar-next-month-btn-day"><div class="ant-calendar-date">'+(i-currentMonthFirstDay-currentMonthLastDay+1)+'</div></td>';
                 } else {
-                    if (month === current.getMonth() && current.getDate() === (i-currentMonthFirstDay+1)) html += '<td class="ant-calendar-cell ant-calendar-today">';
+                    if (this.dateOpt.month === this.dateOpt.curMonth && this.dateOpt.curDate === (i-currentMonthFirstDay+1)) html += '<td class="ant-calendar-cell ant-calendar-today">';
                     else html += '<td class="ant-calendar-cell">';
                     html += '<div class="ant-calendar-date">'+(i-currentMonthFirstDay+1)+'</div></td>';
                 }
@@ -166,21 +215,21 @@
         },
         create: function(target) {
             var only_key = +new Date();
-            var date = new Date();
-            var year = date.getFullYear();
-            var currentMonth = date.getMonth();
             var div = document.createElement('div');
+
             utils.attr(target, Calendar.originOpt.PANELKEY, only_key);
             utils.addClass(target, Calendar.originOpt.PICKERNAME);
-            div.className = Calendar.originOpt.PANELWRAPCLASS;
-            div.innerHTML = TEMPLATE1 + this.getTemplate(year, currentMonth) + TEMPLATE2;
+
+            div.className = Calendar.originOpt.PANELWRAPCLASS + ' ' + Calendar.originOpt.PANELSTR + only_key;
+            div.innerHTML = TEMPLATE1 + this.getTemplate(this.dateOpt.year, this.dateOpt.month) + TEMPLATE2;
+            Calendar.Target = div;
             this.elem_container.appendChild(div);
         },
         openPanel: function(target) {
             if (utils.hasClass(target, Calendar.originOpt.PICKERNAME)) { // 说明该元素已经挂载
                 var only_key = utils.attr(target, Calendar.originOpt.PANELKEY);
-                var panel = document.querySelector('.' + Calendar.originOpt.PANELSTR + only_key);
-                utils.show(panel);
+                Calendar.Target = document.querySelector('.' + Calendar.originOpt.PANELSTR + only_key);
+                utils.show(Calendar.Target);
             } else {
                 this.create(target);
             }
