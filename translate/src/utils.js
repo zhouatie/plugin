2483941c5acdd68021b0685f84f8e5d2e69c7c95
zhouatie <1114349984@qq.com>
2019-11-25 13:47:06 +0800

    feat: 添加句子翻译

--- translate/src/utils.js
+++ translate/src/utils.js
@@ -1,6 +1,9 @@
 const patterns = require('./pattern.js')
 
 const guessIsZh = (value) => patterns.isZh.test(value)
+const formatText = (text, value) => value && text
+
 module.exports = {
-  guessIsZh
+  guessIsZh,
+  formatText
 }
